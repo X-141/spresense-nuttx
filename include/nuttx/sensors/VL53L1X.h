33cbaff3f5b32e576e753340c6d3d7dd6f47fc5d
Sean Phillips <seanphillips141@outlook.com>
2022-04-09 23:32:19 -0400

    Completed initialization methods.

---
+++ include/nuttx/sensors/VL53L1X.h
@@ -0,0 +1,32 @@
+// #ifndef __INCLUDE_NUTTX_SENSORS_MYVL53L1X_H
+// #define __INCLUDE_NUTTX_SENSORS_MYVL53L1X_H
+
+// #include <nuttx/config.h>
+// #include <nuttx/sensors/ioctl.h>
+
+// #if defined(CONFIG_SENSORS_MYVL53L1X) 
+
+// struct i2c_master_s;
+
+// struct VL53L1X_s
+// {
+//   uint16_t distance;
+// };
+
+// #ifdef __cplusplus
+// #define EXTERN extern "C"
+// extern "C"
+// {
+// #else
+// #define EXTERN extern
+// #endif
+
+// int VL53L1X_register(FAR const char *devpath, int port);
+
+// #undef EXTERN
+// #ifdef __cplusplus
+// }
+// #endif
+
+// #endif
+// #endif
