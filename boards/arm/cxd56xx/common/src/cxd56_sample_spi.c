026d381af9911fd48640d509edcb49a0ca3a136b
Sean Phillips <seanphillips141@outlook.com>
2022-05-27 16:11:50 -0400

    Added basic skeleton of how the driver will work. Currently only instatiation and destantiation are defined. Plans to implement transmission are on the way.

---
+++ boards/arm/cxd56xx/common/src/cxd56_sample_spi.c
@@ -0,0 +1,71 @@
+#include <nuttx/config.h>
+#if defined(CONFIG_SPI)
+
+#include <nuttx/board.h>
+#include <nuttx/kmalloc.h>
+
+#include <stdio.h>
+#include <debug.h>
+#include <errno.h>
+
+#include <nuttx/spi/spi.h>
+#include <cxd56_spi.h>
+#include <cxd56_clock.h>
+
+#include <arch/board/cxd56_sample_spi.h>
+
+// Default port value should be 4. (i.e. CONFIG_CXD56_SPI4)
+struct sample_spi_handle *board_sample_spi_initialize(int port, uint8_t mode, uint8_t bit_order, uint32_t frequency)
+{
+    FAR struct spi_dev_s *dev = NULL;
+    dev = cxd56_spibus_initialize(port);
+    if (!dev)
+    {
+        printf("Failed to initialize spibus.\n");
+        return NULL;
+    }
+
+    FAR struct sample_spi_handle *handle = NULL;
+    handle = (struct sample_spi_handle *)kmm_malloc(sizeof(struct sample_spi_handle));
+    if (!handle)
+    {
+        printf("Failed to allocate spi handle for use.\n");
+        return NULL;
+    }
+
+    handle->dev = dev;
+    handle->spi_mode = mode;
+    handle->bit_order = bit_order;
+    handle->clock = frequency;
+
+    return handle;
+}
+
+uint8_t board_sample_spi_uninitialize(struct sample_spi_handle *handle)
+{
+    if (!handle)
+    {
+        printf("Can't deallocate a null handle.\n");
+        return 1;
+    }
+
+    if (handle->dev)
+    {
+        if (SPI_LOCK(handle->dev, false))
+        {
+            printf("Failed to unlock spi bus.\n");
+            return 2;
+        }
+
+        handle->dev = NULL;
+    }
+
+    handle->spi_mode = -1;
+    handle->bit_order = -1;
+    handle->clock = -1;
+    free(handle);
+
+    return 0;
+}
+
+#endif
--- boards/arm/cxd56xx/spresense/include/cxd56_sample_spi.h
+++ boards/arm/cxd56xx/spresense/include/cxd56_sample_spi.h
@@ -3,6 +3,7 @@
 
 #include <nuttx/config.h>
 #include <nuttx/compiler.h>
+#include <nuttx/spi/spi.h>
 #include <stdint.h>
 
 #ifndef __ASSEMBLY__
@@ -16,8 +17,48 @@ extern "C"
 #define EXTERN extern
 #endif
 
-#if defined(CONFIG_SPI)
-int board_sample_spi_initialize(FAR const char* devpath, int port);
+#if defined(CONFIG_SPI) && defined(CONFIG_SENSORS_SAMPLE_SPI)
+
+#define BUFFER_SPACE 64
+
+// Sourced: spresense-arduino-compatible: spi.h
+#define SPI_MODE0 SPIDEV_MODE0  /**< SPI mode 0 */
+#define SPI_MODE1 SPIDEV_MODE1  /**< SPI mode 1 */
+#define SPI_MODE2 SPIDEV_MODE2  /**< SPI mode 2 */
+#define SPI_MODE3 SPIDEV_MODE3  /**< SPI mode 3 */
+
+// Sourced: spresense-arduino-compatible: Arduino.h
+#define LSBFIRST 0x0
+#define MSBFIRST 0x1
+
+struct sample_spi_handle {
+    uint8_t spi_mode;
+    uint8_t bit_order;
+    uint32_t clock;
+    FAR struct spi_dev_s* dev;
+
+    // uint8_t tx_buffer_size;
+    // uint8_t rx_buffer_size;
+
+    // uint8_t tx_buffer[BUFFER_SPACE];
+    // uint8_t rx_buffer[BUFFER_SPACE];
+};
+
+// void clear_buffer_space(struct sample_spi_handle* handle);
+// void set_transfer_buffer(struct sample_spi_handle* handle, uint8_t* data, uint8_t size);
+
+uint16_t transfer_word(struct sample_spi_handle* handle, uint16_t data);
+
+// void perform_transfer(struct sample_spi_handle* handle);
+// uint8_t read_byte(struct sample_spi_handle* handle);
+// uint16_t read_word(struct sample_spi_handle* handle);
+
+struct sample_spi_handle* board_sample_spi_initialize(int port, uint8_t mode, uint8_t bit_order, uint32_t frequency);
+
+uint8_t board_sample_spi_uninitialize(struct sample_spi_handle* handle);
+
+// Consider that we aren't really implementing a character driver. Instead, just
+// an object that we can perform simple functions on.
 #endif
 
 #undef EXTERN
