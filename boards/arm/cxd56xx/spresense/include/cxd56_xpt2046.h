353c36408dc5f45d71587912e5bea2a134f984a1
Sean Phillips <seanphillips141@outlook.com>
2022-06-04 12:16:53 -0400

    Modified xpt2046_handle struct to have a smaller footprint (but less configurability). Removed some #define variables since we will just use what is already enumerated in the spi header file. Shortened function signature for xpt2046 handle init method. Again... a little less configurability, but thats fine for my purposes.

--- boards/arm/cxd56xx/spresense/include/cxd56_xpt2046.h
+++ boards/arm/cxd56xx/spresense/include/cxd56_xpt2046.h
@@ -20,18 +20,6 @@ extern "C"
 #define EXTERN extern
 #endif
 
-
-// Sourced: spresense-arduino-compatible: spi.h
-#define SPI_MODE0 SPIDEV_MODE0  /**< SPI mode 0 */
-// TODO Remove these guys and remove the defined var.
-#define SPI_MODE1 SPIDEV_MODE1  /**< SPI mode 1 */
-#define SPI_MODE2 SPIDEV_MODE2  /**< SPI mode 2 */
-#define SPI_MODE3 SPIDEV_MODE3  /**< SPI mode 3 */
-
-// Sourced: spresense-arduino-compatible: Arduino.h
-#define LSBFIRST 0x0
-#define MSBFIRST 0x1
-
 struct point
 {
     int16_t x;
@@ -42,19 +30,18 @@ struct point
 struct xpt2046_handle
 {
     uint8_t spi_mode;
-    uint8_t bit_order;
     volatile uint8_t isrWake;
+    uint8_t cs_pin;
+    uint8_t irq_pin;
     int16_t zraw;
     int16_t xraw;
     int16_t yraw;
-    uint32_t clock;
-    uint32_t cs_pin;
-    uint32_t tirq_pin;
+    uint32_t clk_freq;
     uint32_t msraw;
     FAR struct spi_dev_s *dev;
 };
 
-struct xpt2046_handle *board_xpt2046_initialize(int port, uint32_t cs_pin, uint32_t irq_pin, uint8_t mode, uint8_t bit_order, uint32_t frequency);
+struct xpt2046_handle *board_xpt2046_initialize(int port, uint32_t cs_pin, uint32_t irq_pin, uint32_t frequency);
 
 uint8_t board_xpt2046_uninitialize(struct xpt2046_handle *handle);
 
